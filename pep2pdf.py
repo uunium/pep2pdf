437d0fafd6e318f7abc221e5a8db9861481ac03c
kharts <kharts@ukr.net>
2015-06-19 17:52:37 +0300

    Add copyright statement

--- pep2pdf.py
+++ pep2pdf.py
@@ -1,1 +1,6 @@
-__author__ = 'kharts'
+# pep2pdf.py - script for downloading the latest PEPs and converting it to PDF
+#
+# Copyright 2015 Andriy Korchak <AndriyKorchak at gmail dot com>
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
