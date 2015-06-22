8d8c0c19aa467de51158da6700d1c0ffd8afc233
kharts <kharts@ukr.net>
2015-06-22 21:00:01 +0300

    Finish converting from pep to html using tempfile

--- pep2pdf.py
+++ pep2pdf.py
@@ -18,6 +18,7 @@ Contains:
 
 import sys
 import urllib2
+import tempfile
 import docutils.core
 
 def pep2pdf(PEP_number, pdf_filename=""):
@@ -35,8 +36,13 @@ def pep2pdf(PEP_number, pdf_filename=""):
     source_file = _get_source_file(PEP_number)
     if source_file is None:
         return False
-    html = docutils.core.publish_file(source=source_file,reader_name="pep",writer_name="pep_html")
-    print html
+    destination = tempfile.NamedTemporaryFile(delete=False)
+    docutils.core.publish_file(source=source_file,
+                                      reader_name="pep",
+                                      writer_name="pep_html",
+                                      destination=destination)
+    # dest_file = open(destination.name)
+    # print dest_file.read()
     return True
 
 def _get_source_file(PEP_number):
