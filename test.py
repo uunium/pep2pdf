33db373effdfdb7cb71fe479ba6b1230a36b2972
kharts <kharts@ukr.net>
2015-06-23 20:22:51 +0300

    Add test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+# test.py - simple test for pep2pdf.py. Downloads all PEPs with
+#   numbers < max_number, and converts it to pdf. Output files
+#   are stored in given directory
+#
+# Copyright 2015 Andriy Korchak <AndriyKorchak at gmail dot com>
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+"""
+A simple test for pep2pdf.py. Downloads all PEPs with
+numbers < max_number, and converts it to pdf. Output files
+are stored in given directory.
+
+Usage:
+
+"""
